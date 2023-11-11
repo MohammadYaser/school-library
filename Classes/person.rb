862d5aa734ec543f4b12da2bc78ef94ac9bbc442
MohammadYaser <yasersafi19@outlook.com>
2023-11-11 18:15:39 +0430

    update person.rb

--- Classes/person.rb
+++ Classes/person.rb
@@ -7,7 +7,7 @@ class Person < Nameable
   attr_reader :id, :rentals
   attr_accessor :name, :age
 
-  def initialize(age, name, parent_permission: true)
+  def initialize(age, name = "Unknown", parent_permission = true)
     super()
     @id = SecureRandom.uuid
     @age = age
